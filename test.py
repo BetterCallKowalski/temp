7b4a7d2e24b792428247aea22871718e12b731a4
root <root@LAPTOP-58MFFE78.localdomain>
2020-01-19 23:28:16 +0300

    test

---
+++ test.py
@@ -0,0 +1,17 @@
+import time
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+chrome = WebDriver(executable_path='/mnt/c/selenium/chromedriver.exe')
+
+chrome.get("https://yandex.ru")
+
+chrome.find_element_by_css_selector('.input__control.input__input').send_keys("большие сиськи")
+
+time.sleep(3)
+
+chrome.find_element_by_css_selector('.button.button_theme_websearch').click()
+
+
+time.sleep(5)
+
+chrome.quit()
